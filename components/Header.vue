2ace00e7a7ad78f77af7a4364cb20d1e068e0179
Ampoi <tange.toshihiro@outlook.jp>
2023-09-24 11:22:44 +0900

    ナビゲーションメニューを作った

--- components/Header.vue
+++ components/Header.vue
@@ -5,10 +5,34 @@
             to="/"
         >海城中高物理部</NuxtLink>
         <div class="grow"/>
+        <div class="sm:flex flex-row items-center gap-4 hidden">
+            <NuxtLink to="/works">Works</NuxtLink>
+            <NuxtLink to="/blogs">Blogs</NuxtLink>
+            <NuxtLink to="https://www.youtube.com/@user-qm9fz9tl4w">Youtube</NuxtLink>
+            <NuxtLink to="https://twitter.com/kaijo_physics">𝕏</NuxtLink>
+            <NuxtLink to="https://www.instagram.com/kaijo_physics/">Instagram</NuxtLink>
+        </div>
+        <button
+            class="sm:hidden"
+            @click="showNavBar = !showNavBar">
+            <Icon name="clarity:menu-line" color="black" size="24"/>
+        </button>
+    </header>
+    <div
+        v-if="showNavBar"
+        class="fixed left-0 top-0 p-8 h-screen w-screen text-lg bg-white shadow-md flex flex-col gap-4 items-center transition-all duration-300">
+        <button
+            class="sm:hidden"
+            @click="showNavBar = !showNavBar">
+            <Icon name="iconamoon:close-thin" color="black" size="24"/>
+        </button>
         <NuxtLink to="/works">Works</NuxtLink>
         <NuxtLink to="/blogs">Blogs</NuxtLink>
         <NuxtLink to="https://www.youtube.com/@user-qm9fz9tl4w">Youtube</NuxtLink>
         <NuxtLink to="https://twitter.com/kaijo_physics">𝕏</NuxtLink>
         <NuxtLink to="https://www.instagram.com/kaijo_physics/">Instagram</NuxtLink>
-    </header>
+    </div>
 </template>
+<script setup lang="ts">
+const showNavBar = ref(false)
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   css: ['~/assets/css/main.css'],
-  modules: ["@nuxt/content"],
+  modules: ["@nuxt/content", "nuxt-icon"],
   postcss: {
     plugins: {
       tailwindcss: {},
